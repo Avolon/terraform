9af50d9c5863fd4d82b83ae876789f201db14463
Avolon <avolon.linux@gmail.com>
2024-03-18 15:34:51 +0300

    terraform

---
+++ create/main.tf
@@ -0,0 +1,56 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "0.61.0"
+    }
+  }
+  backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = "tf-state-bucket-mentor1"
+    region     = "ru-central1-a"
+    key        = "web.tfstate"
+    access_key = ""
+    secret_key = ""
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
+}
+provider "yandex" {
+  service_account_key_file = file ("~/aut.json")
+  cloud_id  = "skill"
+  folder_id = "b1gcapfb425k1p909eqv"
+  zone      = "ru-central1-a"
+}
+
+resource "yandex_vpc_network" "network" {
+  name = "network"
+}
+
+resource "yandex_vpc_subnet" "subnet1" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+resource "yandex_vpc_subnet" "subnet2" {
+  name           = "subnet2"
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.network.id
+  v4_cidr_blocks = ["192.168.11.0/24"]
+}
+
+
+module "ya_instance_1" {
+  source                = "/opt/.modules/instance_1"
+  instance_family_image = "lemp"
+  vpc_subnet_id         = yandex_vpc_subnet.subnet1.id
+}
+
+module "ya_instance_2" {
+  source                = "/opt/.modules/instance_2"
+  instance_family_image = "lamp"
+  vpc_subnet_id         = yandex_vpc_subnet.subnet2.id
+}
